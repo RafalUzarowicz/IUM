85b94124f28d25c6a3be7ce63ce1f1ba87c0ab21
Rafał Uzarowicz <rafal.uzarowicz@gmail.com>
2021-05-23 16:06:57 +0200

    Removed magic literals.

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 #TODO:
 # -testy
 # -moze poprawic te errory ale nie wiem czy jest sens
-# -pousuwac magiczne stale?
 
 from flask import Flask, jsonify
 from flask_restful import Api
 
-from constants import HOST, PORT, MAXIMUM_LOGS_NUMBER, COMPLEX_MODEL_NAME, SIMPLE_MODEL_NAME
+from constants import HOST, PORT, DEFAULT_LOGS_NUMBER, COMPLEX_MODEL_NAME, SIMPLE_MODEL_NAME
 from service.errors import Error
 from service.logger import Logger
 from service.modelresource import ModelResource
@@ -18,7 +17,8 @@ api = Api(app)
 # TODO tutaj uzupelnij modele <3
 simple_m = Model(SIMPLE_MODEL_NAME, "")
 complex_m = Model(COMPLEX_MODEL_NAME, "")
-logger = Logger(MAXIMUM_LOGS_NUMBER)
+logger = Logger()
+
 
 @app.errorhandler(Error)
 def handle_invalid_usage(error):
@@ -26,7 +26,9 @@ def handle_invalid_usage(error):
     response.status_code = error.status_code
     return response
 
-api.add_resource(ModelResource, "/model", "/model/user/<int:user_id>", endpoint="models", resource_class_kwargs={"simple_model": simple_m, "complex_model": complex_m, "logger": logger})
+
+api.add_resource(ModelResource, "/model", "/model/user/<int:user_id>", endpoint="models",
+                 resource_class_kwargs={"simple_model": simple_m, "complex_model": complex_m, "logger": logger})
 
 if __name__ == "__main__":
     app.run(host=HOST, port=PORT, debug=True)
--- service/logger.py
+++ service/logger.py
@@ -4,13 +4,13 @@ from datetime import datetime
 from pathlib import Path
 from threading import Thread
 
-from constants import PROJECT_DIRECTORY
+from constants import PROJECT_DIRECTORY, DEFAULT_LOG_FILES_PREFIX, DEFAULT_LOG_DIR_NAME, LOG_DATETIME_FORMAT, DEFAULT_LOGS_NUMBER
 from service.models import Model
 
 
-def save_to_file(file_name: str, logs: [], dir_name: str = "logs") -> None:
+def save_to_file(file_name: str, logs: [], dir_name: str = DEFAULT_LOG_DIR_NAME) -> None:
     file_dir = os.path.join(PROJECT_DIRECTORY, dir_name)
-    file_name = file_name + "_" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".jsonl"
+    file_name = file_name + "_" + datetime.now().strftime(LOG_DATETIME_FORMAT) + ".jsonl"
     file = os.path.join(file_dir, file_name)
     Path(file_dir).mkdir(parents=True, exist_ok=True)
     with open(file, 'a') as outfile:
@@ -22,7 +22,7 @@ def save_to_file(file_name: str, logs: [], dir_name: str = "logs") -> None:
 
 
 class Logger:
-    def __init__(self, save_iteration: int = 10):
+    def __init__(self, save_iteration: int = DEFAULT_LOGS_NUMBER):
         self.logs_to_save = []
         self.save_iteration = save_iteration
         self.threads_list = []
@@ -35,7 +35,7 @@ class Logger:
     def log(self, model: Model, data: {}, result) -> None:
         # Initialize saving if needed
         if len(self.logs_to_save) >= self.save_iteration:
-            thread = Thread(target=save_to_file, args=("log", self.logs_to_save))
+            thread = Thread(target=save_to_file, args=(DEFAULT_LOG_FILES_PREFIX, self.logs_to_save))
             self.threads_list.append(thread)
             thread.start()
             self.logs_to_save = []
@@ -43,6 +43,6 @@ class Logger:
         self.threads_list = [thread for thread in self.threads_list if thread.is_alive()]
 
         # Log
-        date_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        date_time = datetime.now().strftime(LOG_DATETIME_FORMAT)
         entry = {"timestamp": date_time, "model": model.name, "result": result, "data": data}
         self.logs_to_save.append(entry)
